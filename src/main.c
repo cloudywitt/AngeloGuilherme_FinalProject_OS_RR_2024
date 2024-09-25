3acd952548983945b401025430a991ee27a26a87
Ângelo Garcia <fhzkngj@gmail.com>
2024-09-24 22:56:55 -0400

    Change files/directories data structure

--- src/main.c
+++ src/main.c
@@ -13,8 +13,49 @@
 #include <stdlib.h>
 #include <errno.h>
 
-char directories[256][256];
-int last_dir_idx = -1;
+#define MAX_NAME_SIZE 32
+#define MAX_CHILDREN_NUM 20
+#define MAX_CONTENT_SIZE 126
+
+typedef struct node {
+    char name[MAX_NAME_SIZE];
+    int is_dir;
+    struct node* parent;
+    // if file
+    char** content;
+    // if dir
+    struct node** children;
+    int children_num;
+} Node;
+
+Node* create_node(const char* name, int is_dir) {
+    Node* new_node = malloc(sizeof(Node));
+
+    strcpy(new_node->name, name);
+    new_node->is_dir = is_dir;
+    new_node->parent = NULL;
+    new_node->content = (is_dir) ? NULL : malloc(sizeof(char) * MAX_CONTENT_SIZE);
+    new_node->children = (is_dir) ? calloc(MAX_CHILDREN_NUM, sizeof(Node)) : NULL; // might give some trouble
+    new_node->children_num = (is_dir) ? 0 : -1;
+
+    return new_node;
+}
+
+Node* root;
+
+void init_root() {
+    root = create_node("/", 1);
+}
+
+// find node to getattr
+Node* find_node(const char* path) {
+    if (strcmp("/", path) == 0) return root;
+
+    Node* rt = root;
+
+
+
+}
 
 char files[256][256];
 int last_file_idx = -1;
@@ -22,13 +63,13 @@ int last_file_idx = -1;
 char files_content[256][256];
 int last_file_content_idx = -1;
 
-void print_dir_list() {
-    printf("Dir list:\n");
-
-    for (size_t i = 0; i < 10; i++) {
-        printf("%zu = %s\n", i, directories[i]);
-    }
-}
+// void print_dir_list(Node dir) {
+//     printf("Dir list:\n");
+//
+//     for (size_t i = 0; i < 10; i++) {
+//         printf("%zu = %s\n", i, directories[i]);
+//     }
+// }
 
 void print_file_list() {
     printf("File list:\n");
@@ -37,51 +78,38 @@ void print_file_list() {
         printf("%zu = %s\n", i, files[i]);
     }
 }
-/*
- *
-    */
 
 // dir functions
-int is_dir(const char* path) {
-    if (strcmp(path, "/") == 0) return 1;
 
-    for (int curr_idx = 0; curr_idx <= last_dir_idx; curr_idx++ )
-        if (strcmp(path + 1, directories[curr_idx]) == 0) {
-            return 1;
-        }
+// void add_dir(const char* dir_name) {
+//     for (int i = 0; i < last_dir_idx; i++) {
+//         if (directories[i][0] == '\0') {
+//             strcpy(directories[i], dir_name);
+//
+//             print_dir_list();
+//             return;
+//         }
+//     }
+//
+//     last_dir_idx++;
+//     strcpy(directories[last_dir_idx], dir_name);
+//
+//     print_dir_list();
+// }
 
-    return 0;
-}
-
-void add_dir(const char* dir_name) {
-    for (int i = 0; i < last_dir_idx; i++) {
-        if (directories[i][0] == '\0') {
-            strcpy(directories[i], dir_name);
-
-            print_dir_list();
-            return;
-        }
-    }
-
-    last_dir_idx++;
-    strcpy(directories[last_dir_idx], dir_name);
-
-    print_dir_list();
-}
-
-int rm_dir(const char* dir_name) {
-    for (int i = 0; i <= last_dir_idx; i++) {
-        if (strcmp(dir_name, directories[i]) == 0) {
-            directories[i][0] = '\0';
-
-            if (last_dir_idx == i) {
-                last_dir_idx--;
-            }
-
-            return 1;
-        }
-    }
-}
+// int rm_dir(const char* dir_name) {
+//     for (int i = 0; i <= last_dir_idx; i++) {
+//         if (strcmp(dir_name, directories[i]) == 0) {
+//             directories[i][0] = '\0';
+//
+//             if (last_dir_idx == i) {
+//                 last_dir_idx--;
+//             }
+//
+//             return 1;
+//         }
+//     }
+// }
 
 // file functions
 int is_file(const char* path) {
@@ -154,15 +182,19 @@ static int agfs_getattr(const char* path, struct stat* st) {
 
     printf("agfs_getattr: Path: %s\n", path);
 
-    if (is_dir(path)) {
+    Node* node = find_node(path);
+
+    if (!node) {
+        return -ENOENT; // No file or directory
+    }
+
+    if (node->is_dir) {
         st->st_mode = S_IFDIR | 0755;
         st->st_nlink = 2;
-    } else if (is_file(path)) {
+    } else if (!node->is_dir) {
         st->st_mode = S_IFREG | 0644;
         st->st_nlink = 1;
         st->st_size = 1024;
-    } else {
-        return -ENOENT;
     }
 
     return 0;
@@ -183,31 +215,43 @@ static int agfs_readdir(
     // If the user is trying to show the files/directories of the root directory show the following
     // find the directories of that specific directory and list them
     // this function, readdir, is called upon the directory we're in
-    if (is_dir(path)) {
-        for (int curr_idx = 0; curr_idx <= last_dir_idx; curr_idx++) {
-            if (directories[curr_idx][0] != '\0' && strcmp(path + 1, directories[curr_idx])) {
-                filler(buffer, directories[curr_idx], NULL, 0);
+    Node* directory = find_node(path);
+
+    // might have to check if I can have a dir not found in this case
+    if (directory && directory->is_dir) {
+        // filler every entry of that dir
+
+        for (int i = 0; i < MAX_CHILDREN_NUM; i++) {
+            if (directory->children[i]) {
+                filler(buffer, directory->children[i]->name, NULL, 0);
             }
+
         }
 
-        for (int curr_idx = 0; curr_idx <= last_file_idx; curr_idx++) {
-            if (files[curr_idx][0] != '\0') { // && strcmp(path + 1, files[curr_idx])
-                filler(buffer, files[curr_idx], NULL, 0);
-            }
-        }
+        // for (int curr_idx = 0; curr_idx <= last_dir_idx; curr_idx++) {
+        //     if (directories[curr_idx][0] != '\0' && strcmp(path + 1, directories[curr_idx])) {
+        //         filler(buffer, directories[curr_idx], NULL, 0);
+        //     }
+        // }
+
+        // for (int curr_idx = 0; curr_idx <= last_file_idx; curr_idx++) {
+        //     if (files[curr_idx][0] != '\0') { // && strcmp(path + 1, files[curr_idx])
+        //         filler(buffer, files[curr_idx], NULL, 0);
+        //     }
+        // }
     }
 
     return 0;
 }
 
-static int agfs_rmdir(const char* path) {
-    // after, check if directory is empty. If not, return -ENOTEMPTY
-    if (!rm_dir(path + 1)) {
-        return -ENOENT; // Directory not found
-    }
-
-    return 0;
-}
+// static int agfs_rmdir(const char* path) {
+//     // after, check if directory is empty. If not, return -ENOTEMPTY
+//     if (!rm_dir(path + 1)) {
+//         return -ENOENT; // Directory not found
+//     }
+//
+//     return 0;
+// }
 
 static int agfs_read(
     const char* path,
@@ -228,13 +272,13 @@ static int agfs_read(
     return strlen(content) - offset;
 }
 
-static int agfs_mkdir(const char* path, mode_t mode) {
-    printf("agfs_mkdir: Path: %s\n", path);
-    path++;
-    add_dir(path);
-
-    return 0;
-}
+// static int agfs_mkdir(const char* path, mode_t mode) {
+//     printf("agfs_mkdir: Path: %s\n", path);
+//     path++;
+//     add_dir(path);
+//
+//     return 0;
+// }
 
 static int agfs_mknod(const char* path, mode_t mode, dev_t rdev) {
     printf("agfs_mknod: Path: %s\n", path);
@@ -282,8 +326,8 @@ static struct fuse_operations operations = {
     .getattr = agfs_getattr,
     .readdir = agfs_readdir,
     .read = agfs_read,
-    .mkdir = agfs_mkdir,
-    .rmdir = agfs_rmdir,
+    // .mkdir = agfs_mkdir,
+    // .rmdir = agfs_rmdir,
     .mknod = agfs_mknod,
     .unlink = agfs_unlink,
     .write = agfs_write,
@@ -291,5 +335,7 @@ static struct fuse_operations operations = {
 };
 
 int main(int argc, char* argv[]) {
+    init_root();
+    
     return fuse_main(argc, argv, &operations, NULL);
 }
